c5bda94f739a5296a06c4c11e6d006f968e9fe14
Dave Lowe <mahalowe@gmail.com>
2016-03-10 20:29:16 -0600

    Remove unnecessary import breaking 1.9 compat

--- cart/models.py
+++ cart/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+
 
 class Cart(models.Model):
     creation_date = models.DateTimeField(verbose_name=_('creation date'))
@@ -60,4 +60,3 @@ class Item(models.Model):
         self.object_id = product.pk
 
     product = property(get_product, set_product)
-
